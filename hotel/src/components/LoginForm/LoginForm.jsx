99e0f6c2556e270e9609f0b723a2cb5fc6eb9add
Ruslan Bogdanov <russian.bogd@gmail.com>
2023-04-30 20:10:23 +0300

    feat: Login form
    set up a registration form with formik and stylized it as it was

--- hotel/src/components/LoginForm/LoginForm.jsx
+++ hotel/src/components/LoginForm/LoginForm.jsx
@@ -1,154 +1,91 @@
 import React from "react";
-import { useState } from "react";
+
 import { useDispatch } from "react-redux";
 import { setUser } from "../../redux/slice/userSlice";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 import Button from "../UI/Button/Button";
-// import ErrorMessage from "../UI/ErrorMessage/ErrorMessage";
-import Input from "../UI/Input/Input";
 
 import s from "./LoginForm.module.css";
-import btnStyle from "../UI/Button/Button.module.css";
+import ui from "../UI/uiStyles.module.css";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^[^\u0400-\u04FF]{8,}$/;
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(emailRegex, "Неверный формат почты")
+    .required("Пожалуйста введите почту"),
+  password: Yup.string()
+    .min(8, "Минимальная длина пароля 8 символов")
+    .matches(passwordRegex, "Некорректный пароль")
+    .required("Пожалуйста введите пароль"),
+});
 
 const LoginForm = () => {
-  const [inputLogin, setInputLogin] = useState("");
-  const [inputPassword, setInputPassword] = useState("");
-  const [errorCheckingLogin, setErrorCheckingLogin] = useState(false);
-  const [errorCheckingPassword, setErrorCheckingPassword] = useState(false);
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const passwordRegex = /^[^\u0400-\u04FF]{8,}$/;
-
-  const emailChecking = () => {
-    return emailRegex.test(inputLogin);
-  };
-  const passwordChecking = (password) => {
-    return passwordRegex.test(password);
-  };
-
   const dispatch = useDispatch();
 
-  const handlerSubmit = (event) => {
-    event.preventDefault();
+  const handlerSubmit = (value) => {
+    console.log("авторизация");
+    console.log("login", value);
 
-    setErrorCheckingLogin(true);
-    setErrorCheckingPassword(true);
-    if (emailChecking() && passwordChecking(inputPassword)) {
-      console.log("авторизация");
-      dispatch(
-        setUser({
-          isAuth: true,
-          login: inputLogin,
-        })
-      );
-      localStorage.setItem("isAuth", true);
-      localStorage.setItem("login", inputLogin);
+    const auth = {
+      isAuth: true,
+      login: value.email,
+    };
 
-      setErrorCheckingLogin(false);
-      setErrorCheckingPassword(false);
-    }
+    dispatch(setUser(auth));
+    localStorage.setItem("isAuth", true);
+    localStorage.setItem("login", value.email);
   };
 
   return (
     <div className={s.box}>
       <div className={s.content}>
         <h2 className={s.title}>Simple Hotel Check</h2>
-        {/* <form onSubmit={handlerSubmit}>
-          <div className={s.inputs}>
-            <div className={s.wrapperInput}>
-              <Input
-                title={"Логин"}
-                value={inputLogin}
-                onChange={setInputLogin}
-              />
-              {errorCheckingLogin && !inputLogin && (
-                <ErrorMessage message="Пожалуйста введите почту" />
-              )}
-              {errorCheckingLogin && inputLogin && !emailChecking() && (
-                <ErrorMessage message="Неверный формат почты" />
-              )}
-            </div>
-            <div className={s.wrapperInput}>
-              <Input
-                title={"Пароль"}
-                value={inputPassword}
-                onChange={setInputPassword}
-                type={"password"}
-              />
-              {errorCheckingPassword && !inputPassword && (
-                <ErrorMessage message="Пожалуйста введите пароль" />
-              )}
-              {errorCheckingPassword &&
-                inputPassword &&
-                !passwordChecking(inputPassword) && (
-                  <ErrorMessage message="Некоректный пароль" />
-                )}
-            </div>
-          </div>
-          <Button type="submit">Войти</Button>
-        </form> */}
         <Formik
           initialValues={{ email: "", password: "" }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = "Required-Пожалуйста введите почту";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address-Неверный формат почты";
-            }
-
-            if (!values.password) {
-              errors.password = "Required-Введите пароль";
-            } else if (!passwordRegex.test(values.password)) {
-              let message = "Неверный формат пароля";
-              if (values.password.length < 8) {
-                message += "Пароль должен быть не меньше 8 символов";
-              }
-              errors.password = message;
-            }
-
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              console.log(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            handlerSubmit(values);
           }}
         >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-            <>
-              <Form>
-                <div className={s.inputs}>
-                  <div className={s.wrapperInput}>
-                    <Field type="email" name="email" />
-                    {/* <ErrorMessage name="email" component="div" /> */}
+          {({ isSubmitting }) => (
+            <Form>
+              <div className={s.inputs}>
+                <div className={s.wrapperInput}>
+                  <div className={ui.boxInput}>
+                    <p className={ui.title}>Логин</p>
+                    <Field type="email" name="email" className={ui.input} />
                   </div>
-                  <div className={s.wrapperInput}>
-                    <Field type="password" name="password" />
-                    {/* <ErrorMessage name="password" component="div" /> */}
-                  </div>
+                  <ErrorMessage
+                    name="email"
+                    component={"p"}
+                    className={ui.errorMessage}
+                  />
                 </div>
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className={btnStyle.btn}
-                >
-                  Войти
-                </button>
-              </Form>
-            </>
+                <div className={s.wrapperInput}>
+                  <div className={ui.boxInput}>
+                    <p className={ui.title}>Пароль</p>
+                    <Field
+                      type="password"
+                      name="password"
+                      className={ui.input}
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="password"
+                    component={"p"}
+                    className={ui.errorMessage}
+                  />
+                </div>
+              </div>
+
+              <Button type="submit" disabled={isSubmitting}>
+                Войти
+              </Button>
+            </Form>
           )}
         </Formik>
       </div>
